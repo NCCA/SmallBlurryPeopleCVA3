1e666784be3725b8a983b6dc3d99aec1b7611b97
Quentin Corker-Marin <i7624405@bournemouth.ac.uk>
2016-11-03 15:06:19 +0000

    ngl now working

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 #include <SDL2/SDL.h>
 #include "ngl/Vec3.h"
+#include "ngl/NGLStream.h"
 
 int main()
 {
     SDL_Window *w = SDL_CreateWindow("test", 100, 100, 100, 100, SDL_WINDOW_SHOWN);
     SDL_Delay(1000);
-    std::cout << "hello\n";
+    ngl::Vec3 a(1, 2, 3);
+    std::cout << "hello\n" << a << "\n";
     return 0;
 }
