5590fdcd423662e464b8638ffb130be52479d8f0
Felix Marrington-Reeve <i7621149@bournemouth.ac.uk>
2016-11-18 14:41:42 +0000

    basic character class start

--- src/Character.cpp
+++ src/Character.cpp
@@ -5,14 +5,18 @@
 Character::Character(Grid *_grid):
   m_grid(_grid)
 {
-  ngl::Random rand = ngl::Random::instance();
+  ngl::Random *rand = ngl::Random::instance();
   rand->setSeed(0);
 
   int target_x = (int)rand->randomPositiveNumber(10);
   int target_y = (int)rand->randomPositiveNumber(10);
+  ngl::Vec2 target_pos = ngl::Vec2(target_x, target_y);
 
-  // m_target_id = grid->getTileId(target_x, target_y);
-  // m_grid->getTilePos(tile_id, x_pos, y_pos);
+  m_target_id = m_grid->coordToId(target_pos);
+  target_pos = m_grid->idToCoord(m_target_id);
+
+
+  update();
 }
 
 void Character::update()
