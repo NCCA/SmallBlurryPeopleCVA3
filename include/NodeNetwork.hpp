20b1af19034e2fc038271bda1fdf37f5fb5034ee
Felix Marrington-Reeve <i7621149@bournemouth.ac.uk>
2016-12-15 15:07:01 +0000

    node network beginning to take over pathfinding

--- include/NodeNetwork.hpp
+++ include/NodeNetwork.hpp
@@ -1,9 +1,38 @@
 #ifndef __NODENETWORK_HPP__
 #define __NODENETWORK_HPP__
 
+#include "Node.hpp"
+#include "Grid.hpp"
+#include <vector>
+#include "ngl/Vec2.h"
+
+/// @file NodeNetwork.hpp
+/// @brief The NodeNetwork can be created as a temporary helper object for finding a path.
+/// all pathfinding logic is contained within it
+
+/// @class NodeNetwork
+/// @brief Wraps up a Node vector and uses it to find a path on the Grid object given in its constructor
+
 class NodeNetwork
 {
+public:
+  /// @brief Constructor requires a grid and a start and end point for pathfinding
+  /// @param [in] _grid the grid used to check tile traversability
+  /// @param [in] _pos initial position of character
+  /// @param [in] _target_pos target position to find a path to
+  NodeNetwork(Grid *_grid, ngl::Vec2 _pos, ngl::Vec2 _target_pos);
 
+  /// @brief findPath returns a vector of 2d points that can be traversed
+  /// @return vector of points in a path to target, or 0 length vector if no path is found
+  std::vector<ngl::Vec2> findPath();
+
+private:
+  /// @brief grid which network can read for pathfinding
+  Grid *m_grid;
+  /// @brief initial position of character
+  ngl::Vec2 m_char_pos;
+  /// @breif target position of character
+  ngl::Vec2 m_target_pos;
 };
 
 #endif//__NODENETWORK_HPP__
---
+++ src/NodeNetwork.cpp
@@ -0,0 +1,154 @@
+#include "NodeNetwork.hpp"
+
+NodeNetwork::NodeNetwork(Grid *_grid, ngl::Vec2 _pos, ngl::Vec2 _target_pos) :
+  m_grid(_grid),
+  m_char_pos(_pos),
+  m_target_pos(_target_pos)
+{
+
+}
+
+std::vector<ngl::Vec2> NodeNetwork::findPath()
+{
+  bool path_found = false;
+  while(!path_found)
+  {
+
+    /// FUNCTION: GET LOWEST COST
+    // pointer to current node
+    int lowest_f_node_id = -1;
+
+    // global open flag = false
+    bool open_nodes_available = false;
+    // for each node in nodes:
+    for(Node &node : m_nodes)
+    {
+      // if open:
+      if(node.isOpen())
+      {
+        open_nodes_available = true;
+        // compare node cost to current lowest
+        if(lowest_f_node_id == -1)
+        {
+          lowest_f_node_id = node.getID();
+        }
+        else if (node.getFCost() < nodes[lowest_f_node_id].getFCost())
+        {
+          // update lowest cost node
+          lowest_f_node_id = node.getID();
+        }
+      }
+    }
+
+    // if no nodes are open
+    if(!open_nodes_available)
+    {
+      std::cout << "no path found!" << std::endl;
+      break;
+    }
+
+    ///FUNCTION END
+
+    ///FUNCTION GET NEIGHBOURS AND UPDATE COSTS
+
+    // create neighbour list, (4)
+    std::array<int, 4> neighbour_ids = nodes[lowest_f_node_id].getNeighbours(&nodes);
+    // check for neighbours
+    // place neighbours that are not yet in the vector
+    int current_tile_id = nodes[lowest_f_node_id].getTileID();
+    ngl::Vec2 current_pos = m_grid->idToCoord(current_tile_id);
+
+    for(size_t i = 0; i < neighbour_ids.size(); i++)
+    {
+      // if neighbour is not found
+      if(neighbour_ids[i] == -1)
+      {
+        int tile_id = -1;
+        switch(i)
+        {
+          case Neighbour::UP:
+            if(current_pos[1] > 0)
+            {
+              tile_id = current_tile_id - m_grid->getW();
+            }
+            break;
+          case Neighbour::DOWN:
+            if(current_pos[1] < m_grid->getH() - 1)
+            {
+              tile_id = current_tile_id + m_grid->getW();
+            }
+            break;
+          case Neighbour::LEFT:
+            if(current_pos[0] > 0)
+            {
+              tile_id = current_tile_id - 1;
+            }
+            break;
+          case Neighbour::RIGHT:
+            if(current_pos[0] < m_grid->getW() - 1)
+            {
+              tile_id = current_tile_id + 1;
+            }
+            break;
+          default:
+            std::cerr << "tile id not found" << std::endl;
+        }
+        if(tile_id != -1)
+        {
+          neighbour_ids[i] = nodes.size();
+          nodes.push_back(Node(m_grid, &nodes, tile_id, m_target_id, nodes[lowest_f_node_id].getID()));
+        }
+      }
+    }
+
+    // for each neighbour:
+    for(int neighbour_id : neighbour_ids)
+    {
+      if(neighbour_id != -1)
+      {
+        Node *neighbour = &(nodes[neighbour_id]);
+        // if open:
+        if(neighbour->isOpen())
+        {
+          // check G cost
+          if(neighbour->calcGCost(&nodes, nodes[lowest_f_node_id].getID()) < neighbour->getGCost())
+          {
+            // if lower, update parent to current node
+            neighbour->setParent(&nodes, nodes[lowest_f_node_id].getID());
+            //std::cout << neighbour->getParentID() << std::endl;
+          }
+        }
+      }
+    }
+
+    /// CLOSE NODE
+    // close current node
+    nodes[lowest_f_node_id].close();
+
+
+    // if current is target node, path is found
+    if(nodes[lowest_f_node_id].getTileID() == m_target_id)
+    {
+      ///FUNCTION CREATE PATH
+
+      path_found = true;
+      // create path with nodes
+      //std::cout << "character position" << m_pos << std::endl;
+      //std::cout << "character target" << m_grid->idToCoord(m_target_id) << std::endl;
+      //std::cout << "path found!" << std::endl;
+      Node *current_node = &(nodes[lowest_f_node_id]);
+      while(true)
+      {
+        m_path.push_back(current_node->getPos());
+        if(current_node->getParentID() == -1)
+        {
+          break;
+        }
+        current_node = &(nodes[current_node->getParentID()]);
+      }
+
+      int nMilliseconds = myTimer.elapsed();
+      std::cout << "time taken" << nMilliseconds << std::endl;
+    }
+  }
+}
