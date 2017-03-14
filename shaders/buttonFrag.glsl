29cc31fa91e8dd679fc86203e02d9001bf226de8
Felix Marrington-Reeve <i7621149@bournemouth.ac.uk>
2017-03-14 11:59:49 +0000

    rounded buttons in shader

--- shaders/buttonFrag.glsl
+++ shaders/buttonFrag.glsl
@@ -11,18 +11,40 @@ in vec2 fragSize;
 in vec2 fragUV;
 in float fragMousedOver;
 
-float border_size = 5;
+float border_size = 7;
+vec3 border_color = vec3(1,1,0);
+vec3 button_color = vec3(0.7, 0.1, 0.0);
+vec3 button_highlight = vec3(0.9, 0.7, 0.4);
+
+vec3 button_selected = vec3(0.1, 0.2, 0.2);
 
 void main()
 {
   //vec2 buttonUV = (gl_FragCoord.xy - posCoord) / sizeCoord;
-  vec3 border_color = vec3(1,1,0);
+  vec2 button_pixel_size = fragSize * fResolution;
+  vec2 button_pixel_pos = fragPos * fResolution;
+  vec2 pixel_uv = fragUV * button_pixel_size;
+
   vec3 s = border_color;
-  vec2 button_inner = 0.5 - (border_size * 2) / (fragSize*fResolution);
-  float radius = border_size;
-  if(box(translate(fragUV, vec2(0.5)), button_inner, 0) <= 0)
+  vec2 button_inner = button_pixel_size/2 - border_size;
+
+
+  if(box(translate(pixel_uv, button_pixel_size/2), button_pixel_size/2, border_size*3) > 0)
   {
-    s = vec3(0,0,1);
+    discard;
+  }
+
+  if(fragMousedOver > 0)
+  {
+    button_color += button_selected;
+    button_highlight += button_selected;
+  }
+//  vec2 button_inner = 0.5 - (border_size * 2) / (fragSize*fResolution);
+//  float radius = border_size;
+//  if(box(translate(fragUV, vec2(0.5)), button_inner, 0) <= 0)
+  if(box(translate(pixel_uv, button_pixel_size/2), button_inner, border_size*2) <= 0)
+  {
+    s = mix(button_highlight, button_color, fragUV.y);
   }
 
   outColour = vec4(s, 1.0);
