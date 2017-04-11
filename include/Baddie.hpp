6539c361ef00c96fcdcbcccd73ed753d589fd7e6
Felix Marrington-Reeve <i7621149@bournemouth.ac.uk>
2017-04-11 15:45:43 +0100

    baddie boilerplate

---
+++ include/Baddie.hpp
@@ -0,0 +1,72 @@
+#ifndef __BADDIE_HPP__
+#define __BADDIE_HPP__
+
+#include "ngl/Vec2.h"
+#include "ngl/Vec3.h"
+#include "Grid.hpp"
+#include "vector"
+
+class Baddie
+{
+public:
+  ///
+  /// \brief Baddie constructor to create baddie at edge of map
+  /// \param _grid grid to use
+  ///
+  Baddie(Grid *_grid);
+  ///
+  /// \brief default destructor
+  ///
+  ~Baddie() = default;
+  ///
+  /// \brief update calculate behaviours for baddie
+  ///
+  void update();
+  ///
+  /// \brief move, moves baddie along its path
+  /// \return true if baddie reaches target
+  ///
+  bool move();
+  ///
+  /// \brief calcAimVec, calculate vector towards next point
+  /// \return vector to target
+  ///
+  ngl::Vec2 calcAimVec(float *dist_squared);
+
+	///
+	/// \brief setTarget, set a new target position based on a position
+	/// \param _target_pos, the position to pathfind to
+	///
+	bool setTarget(ngl::Vec2 _target_pos);
+
+	void findPath(ngl::Vec2 _target);
+
+	ngl::Vec3 getPos();
+private:
+  ///
+  /// \brief m_pos position of baddie
+  ///
+  ngl::Vec2 m_pos;
+  ///
+  /// \brief m_grid pointer to grid baddie is on
+  ///
+  Grid *m_grid;
+  ///
+  /// \brief m_health float between 0..1 for health value
+  ///
+  float m_health;
+  ///
+  /// \brief m_speed speed of character
+  ///
+  float m_speed;
+  ///
+  /// \brief m_path, vector of target positions for movement
+  ///
+  std::vector<ngl::Vec2> m_path;
+  ///
+  /// \brief m_target_id target tile id
+  ///
+  int m_target_id;
+};
+
+#endif//__BADDIE_HPP__
---
+++ src/Baddie.cpp
@@ -0,0 +1,113 @@
+#include "Baddie.hpp"
+#include "NodeNetwork.hpp"
+#include "Prefs.hpp"
+#include "ngl/NGLStream.h"
+
+Baddie::Baddie(Grid *_grid) :
+  m_grid(_grid),
+  m_path()
+{
+  m_pos = ngl::Vec2(0.5,0.5);
+  m_target_id = m_grid->coordToId(m_pos);
+  m_health = 1.0f;
+  Prefs* prefs = Prefs::instance();
+  m_speed = prefs->getFloatPref("CHARACTER_SPEED") * 0.8;
+
+  findPath(ngl::Vec2(m_grid->getW()/2, m_grid->getH()/2));
+}
+
+void Baddie::update()
+{
+  if(move())
+  {
+    //newRandomTarget();
+  }
+
+  //setTarget(target);
+
+  //std::cout << "-Baddie-" << std::endl;
+  //std::cout << "pos:" << m_pos << std::endl;
+}
+
+bool Baddie::move()
+{
+  // check whether the next point is necessary or if character has line of sight to the second point in list
+  if(m_path.size() > 1 && NodeNetwork::raytrace(m_grid, m_pos, m_path.end()[-2]))
+  {
+    // if character can see point after next, remove the next point so character heads directly to the furtherst one they can see
+    // done on a one by one basis so it does all happen in the same frame as pathfinding, not super slow that way but marginally better this way
+    m_path.pop_back();
+  }
+
+  // move by distance up to speed
+  float dist_moved = 0;
+  while(m_path.size() > 0 && dist_moved < m_speed)
+  {
+    float dist = 1;
+    ngl::Vec2 aim_vec = calcAimVec(&dist);
+    if(dist < m_speed)
+    {
+      aim_vec *= dist;
+      m_path.pop_back();
+    }
+    else
+    {
+      aim_vec *= m_speed;
+    }
+    dist_moved += aim_vec.length();
+    m_pos += aim_vec;
+  }
+
+  if(m_path.size() <= 0)
+  {
+    return true;
+  }
+  else return false;
+}
+
+ngl::Vec2 Baddie::calcAimVec(float *dist)
+{
+  ngl::Vec2 aim_vec(0,0);
+  if(m_path.size() > 0)
+  {
+   aim_vec = m_path.back() - m_pos;
+  }
+  if(dist)
+  {
+    *dist = aim_vec.length();
+  }
+  if(aim_vec != ngl::Vec2(0,0))
+  {
+    aim_vec.normalize();
+  }
+  return aim_vec;
+}
+
+bool Baddie::setTarget(ngl::Vec2 _target_pos)
+{
+  if(m_target_id != m_grid->coordToId(_target_pos))
+  {
+    findPath(_target_pos);
+  }
+  if(m_path.size() <= 0)
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
+}
+
+void Baddie::findPath(ngl::Vec2 _target)
+{
+  NodeNetwork network(m_grid, m_pos, _target);
+  m_path = network.findPath();
+}
+
+ngl::Vec3 Baddie::getPos()
+{
+  //get grid height at baddie's position
+  float height = m_grid->getInterpolatedHeight(m_pos[0], m_pos[1]);
+  return ngl::Vec3(m_pos[0], height, m_pos[1]);
+}
