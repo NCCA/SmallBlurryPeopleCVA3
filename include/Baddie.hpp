ceaedd7042faf09e7bf5e6747adddf941c09ef90
REmery <i7679227@bournemouth.ac.uk>
2017-05-09 17:31:13 +0100

    added doxygen comments

--- include/Baddie.hpp
+++ include/Baddie.hpp
@@ -9,101 +9,103 @@
 
 #include <vector>
 
-/// \file Baddie.hpp
-/// \brief The enemy class that wanders around searching for characters, when one comes into range
+/// @file Baddie.hpp
+/// @brief The enemy class that wanders around searching for characters, when one comes into range
 ///  it follows and attacks the character
 
 
 class Character;
 
-/// \class Baddie
-/// \brief The Baddie class is the ingame enemy, with attacking and tracking states
+/// @class Baddie
+/// @brief The Baddie class is the ingame enemy, with attacking and tracking states
 ///
 class Baddie : public AI
 {
 public:
   ///
-  /// \brief Baddie constructor to create baddie at edge of map
-  /// \param _grid grid to use
+	/// @brief ctr, create baddie with a position on the grid and a references to the characters
+	/// @param [in]_grid, pointer to grid for pathfinding and positions
   ///
 	Baddie(TerrainHeightTracer *_height_tracer, Grid *_grid, std::vector<Character> *_characters);
   ///
-  /// \brief default destructor
+	/// @brief default destructor
   ///
   ~Baddie() = default;
   ///
-  /// \brief update calculate behaviours for baddie
+	/// @brief update calculate behaviours for baddie
   ///
 	void update();
 	///
-	/// \brief trackingState, baddie intiates fight with character and follows it
+	/// @brief trackingState, baddie intiates fight with character and follows it
 	///
 	void trackingState();
 	///
-	/// \brief invadedState, character intiates fight with baddie
+	/// @brief invadedState, character intiates fight with baddie
 	///
 	void invadedState(Character *_target);
 	///
-	/// \brief setInvade, stop character looking for an enemy
+	/// @brief setInvade, stop character looking for an enemy
 	///
 	void stopSearching() {m_search = false;}
 	///
-	/// \brief fight, baddies actions when fighting
+	/// @brief fight, baddies actions when fighting
 	///
 	void fight();
 	///
-	/// \brief idleState, sets baddie to not be in a fithing state
+	/// @brief idleState, sets baddie to not be in a fithing state
 	///
 	void idleState() {m_combat = false;}
 	///
-	/// \brief findNearestTarget, finds character nearest to baddie
+	/// @brief findNearestTarget, finds character nearest to baddie
+	/// @return a boolean determining whether a viable target was found
 	///
 	bool findNearestTarget();
 	///
-	/// \brief getScale, returns baddie's scale for drawing
-	/// \return m_scale, scale of baddie
+	/// @brief getScale, returns baddie's scale for drawing
+	/// @return m_scale, scale of baddie
 	///
 	float getScale() {return m_scale;}
 	///
-	/// \brief addScale, adds onto scale of baddie
-	/// \param _scale, amount to add onto scale
+	/// @brief addScale, adds onto scale of baddie
+	/// @param _scale, amount to add onto scale
 	///
 	void addScale(float _scale) {m_scale += _scale;}
+
 private:
 	///
-	/// \brief m_combat, if the character is in combat
+	/// @brief m_combat, if the character is in combat
 	///
 	bool m_combat;
 	///
-	/// \brief m_invaded, if the character is being attacked, wont look for other enemies
+	/// @brief m_invaded, if the character is being attacked, wont look for other enemies
 	///
 	bool m_search;
 	///
-	/// \brief m_track, if the character is following character
+	/// @brief m_track, if the character is following character
 	///
 	bool m_track;
 	///
-	/// \brief m_characters, vector of characters
+	/// @brief m_characters, vector of characters
 	///
 	std::vector<Character> *m_characters;
 	///
-	/// \brief m_targets, current character target's
+	/// @brief m_targets, current character target's
 	///
 	std::vector<Character*> m_targets;
 	///
-	/// \brief m_attack_power, how much the baddie attacks for
+	/// @brief m_attack_power, how much the baddie attacks for
 	///
 	float m_attack_power;
 	///
-	/// \brief m_agro_distance, distance a character can move where the baddie will stop pursuing
+	/// @brief m_agro_distance, distance a character can move where the baddie will stop pursuing
 	///
 	float m_agro_distance;
 	///
-	/// \brief m_tracking_distance, range a character needs to be in to target
+	/// @brief m_tracking_distance, range a character needs to be in to target
 	///
 	float m_tracking_distance;
 	///
-	/// \brief m_scale, baddie grows in size when it kills enemies
+	/// @brief m_scale, baddie grows in size when it kills enemies
 	///
 	float m_scale;
 };
--- include/Inventory.hpp
+++ include/Inventory.hpp
@@ -1,29 +1,86 @@
 #ifndef __INVENTORY_HPP__
 #define __INVENTORY_HPP__
 
+/// @file Inventory.hpp
+/// @brief The world inventory, globally shared across storehouses
+///
+
+/// @class Inventory
+/// @brief The Inventory class for management of the global inventory as accessed by the character through storehouses
+///
 class Inventory
 {
 	public:
+		///
+		/// @brief ctor, sets up intial values of inventory items
+		///
 		Inventory();
+		///
+		/// @brief destructor
+		///
 		~Inventory() = default;
-
+		///
+		/// @brief getWoodInventory, get the amount of wood form the global inventory
+		/// @return m_wood_inventory, amount of wood in the inventory
+		///
 		int getWoodInventory() {return m_wood_inventory;}
+		///
+		/// @brief getBerryInventory, get the amount of berries from the global inventory
+		/// @return m_berry_inventory, amount of berries in the inventory
+		///
 		int getBerryInventory() {return m_berry_inventory;}
+		///
+		/// @brief getFishInventory, get the amount of fishes from the global inventory
+		/// @return m_fish_inventory, amount of fishes in the inventory
+		///
 		int getFishInventory() {return m_fish_inventory;}
-
+		///
+		/// @brief addWood, add wood to the global inventory
+		/// @param _amount, amount of wood being added to the inventory
+		///
 		void addWood(int _amount);
+		///
+		/// @brief addBerries, add berries to the global inventory
+		/// @param [in] _amount, amount of berries being added to the inventory
+		///
 		void addBerries(int _amount);
+		///
+		/// @brief addFish, add fishes to the global inventory
+		/// @param [in] _amount, amount of fishes being added to the inventory
+		///
 		void addFish(int _amount);
-
+		///
+		/// @brief takeWood, take wood from the global inventory
+		/// @param [in] _amount, amount of wood wanting to be taken
+		/// @return amount of wood available/the amount asked for
+		///
 		int takeWood(int _amount);
+		///
+		/// @brief takeBerries, take berries from the global inventory
+		/// @param [in] _amount, amount of berries wanting to be taken
+		/// @return amount of berries available/the amount asked for
+		///
 		int takeBerries(int _amount);
+		///
+		/// @brief takeFish, take fish from the global inventory
+		/// @param [in] _amount, amount of fishes wanting to be taken
+		/// @return amount of fishes available/ the amount asked for
+		///
 		int takeFish(int _amount);
 
 	private:
+		///
+		/// @brief m_wood_inventory, amount of wood in the global inventory
+		///
 		int m_wood_inventory;
+		///
+		/// @brief m_berry_inventory, amount of berries in the global inventory
+		///
 		int m_berry_inventory;
+		///
+		/// @brief m_fish_inventory, amount of fishes in the global inventory
+		///
 		int m_fish_inventory;
-
 };
 
 #endif//__INVENTORY_HPP__
